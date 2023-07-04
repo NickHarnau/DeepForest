f7021cd94e9b997fc08f999b2e2360e1255fd575
Nick <nick.harnau@imw.fraunhofer.de>
2023-07-04 12:56:17 +0200

    fix bug

--- deepforest/evaluate.py
+++ deepforest/evaluate.py
@@ -150,10 +150,10 @@ def evaluate(predictions, ground_df, root_dir, iou_threshold=0.4, savedir=None, 
     results = pd.concat(results)
     box_precision = 0
     box_recall = 0
-    if isinstance(predictions.shape[0], int) and isinstance(sum(results["match"]), int): # check if predictions were made
+    if predictions.shape[0] is not None and sum(results["match"]) is not None: # check if predictions were made
         box_precision = sum(results["match"]) / predictions.shape[0]
-    if isinstance(sum(results["match"]), int) and isinstance(results.shape[0], int): # check if
-        box_recall = sum(results["match"]) / results.shape[0]#use of results df, because this is always displaying the ground truth data
+    if sum(results["match"]) is not None and results.shape[0] is not None: # check if
+        box_recall = sum(results["match"]) / results.shape[0] #use of results df, because this is always displaying the ground truth data
 
     if average:
         box_precision = np.mean(box_precisions)
